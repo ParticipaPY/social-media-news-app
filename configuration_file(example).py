7839ca26c8dd14e5349cb89f2cccdf798035c3f5
Marcelo <marce.mmad@gmail.com>
2016-08-06 10:11:12 -0400

    Added an example of the config file

---
+++ configuration_file(example).py
@@ -0,0 +1,25 @@
+#IMPORTANT
+#this is an example of the module 'configuration_file.py'. In order to use it make a copy, fullfill the parameter values and rename the file to 'configuration_file.py'
+
+
+# see the readme file to know how to get these values
+# facebook_access_token = "app_id|app_secret"
+facebook_access_token = "copy you access token here"
+
+# to get a fanpage id go to http://findmyfbid.com/
+facebook_sources = {'ABC Color': '280037675322', 'Ultima Hora': '48880688722', 'La Nacion': '132554373464255'}
+
+# age (in days) of both, facebook posts and tweets.
+days = 1
+
+###########################################################################################################################################
+
+# see the readme file to konw how to get these values
+tw_access_token    = 'copy your access token here'
+tw_access_secret   = 'copy your access secret here'
+tw_consumer_key    = 'copy your consumer key here'
+tw_consumer_secret = 'copy your consumer secret here'
+
+
+# to get a twitter account id go to https://tweeterid.com/
+tw_sources = {'ABC Color' : 28191953, 'Ultima Hora' : 30054530, 'La Nacion' : 33989170}
